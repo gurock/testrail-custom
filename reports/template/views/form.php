12a52f8e4658b2ab8dc638aed188a60fe9f41d41
Tobias Gurock <tg@gurock.com>
2013-08-06 15:26:39 +0200

    Added template report

---
+++ reports/template/views/form.php
@@ -0,0 +1,20 @@
+<? $tab = 1 ?>
+
+<p><?= lang('reports_tmpl_form_intro') ?></p>
+
+<div class="tabs">
+	<div class="tabHeader">
+		<a href="javascript:void(0)" class="tab1 <?= $tab == 1 ? 'current' : '' ?>" rel="1"
+			onclick="App.Tabs.activate(this)"><?= lang('reports_tmpl_form_tab1') ?></a>
+		<a href="javascript:void(0)" class="tab2 <?= $tab == 2 ? 'current' : '' ?>" rel="2"
+			onclick="App.Tabs.activate(this)"><?= lang('reports_tmpl_form_tab2') ?></a>
+	</div>
+	<div class="tabBody">
+		<div class="tab tab1 <?= $tab != 1 ? 'hidden' : '' ?>">
+			<?= lang('reports_tmpl_form_tab1') ?>
+		</div>
+		<div class="tab tab2 <?= $tab != 2 ? 'hidden' : '' ?>">
+			<?= lang('reports_tmpl_form_tab2') ?>
+		</div>
+	</div>
+</div>
---
+++ reports/template/views/index.php
@@ -0,0 +1,31 @@
+<?php
+$min_width = 960;
+
+$header = array(
+	'project' => $project,
+	'report' => $report,
+	'meta' => $report_obj->get_meta(),
+	'min_width' => $min_width,
+	'css' => array(
+		'styles/reset.css' => 'all',
+		'styles/view.css' => 'all',
+		'styles/print.css' => 'print'
+	),
+	'js' => array(
+		'js/jquery.js'
+	)
+);
+
+$GI->load->view('report_plugins/layout/header', $header);
+?>
+
+<p><?= lang('reports_tmpl_intro') ?></p>
+
+<?php
+$temp = array();
+$temp['report'] = $report;
+$temp['meta'] = $report_obj->get_meta();
+$temp['show_options'] = true;
+$temp['show_report'] = true;
+$GI->load->view('report_plugins/layout/footer', $temp);
+?>
